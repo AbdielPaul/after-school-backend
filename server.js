52de3137090e20225e478c16b38166a8de48f858
AbdielPaul <abdielpaul29@gmail.com>
2025-11-23 20:00:24 +0400

    Add Express server

--- server.js
+++ server.js
@@ -5,6 +5,14 @@ const app = express();
 app.use(express.json());
 const port = process.env.PORT || 3000;
 
+// Root route
+app.get('/', (req, res) => {
+    res.json({ 
+        message: 'Webstore API is running',
+        version: '1.0.0'
+    });
+});
+
 app.listen(port, () => {
     console.log(`Express.js API server running at localhost:${port}`);
     
