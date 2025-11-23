cb906c8d885633d4fd51d22df8ae0f924a1a9e7f
AbdielPaul <abdielpaul29@gmail.com>
2025-11-23 20:06:20 +0400

    Configure CORS middleware

--- server.js
+++ server.js
@@ -5,6 +5,15 @@ const app = express();
 app.use(express.json());
 const port = process.env.PORT || 3000;
 
+// CORS middleware
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Credentials', 'true');
+    res.setHeader('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT,DELETE');
+    res.setHeader('Access-Control-Allow-Headers', "Access-Control-Allow-Headers, Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
+    next();
+});
+
 const MongoClient = require('mongodb').MongoClient;
 const ObjectID = require('mongodb').ObjectID;
  
