859c27093bbc8f46496c46c2a9cb3214973b5d21
AbdielPaul <abdielpaul29@gmail.com>
2025-11-23 20:03:28 +0400

    Integrate MongoDB connection

--- server.js
+++ server.js
@@ -5,6 +5,20 @@ const app = express();
 app.use(express.json());
 const port = process.env.PORT || 3000;
 
+const MongoClient = require('mongodb').MongoClient;
+const ObjectID = require('mongodb').ObjectID;
+ 
+let db;
+
+MongoClient.connect('mongodb+srv://abdielpaul29_db_user:SNNNgZ4njPF3WqKS@cluster0.sypziud.mongodb.net/', (err, client) => { 
+    if (err) {
+        console.error('MongoDB connection error:', err);
+        return;
+    }
+    db = client.db('webstore');
+    console.log('Connected to MongoDB');
+});
+
 // Root route
 app.get('/', (req, res) => {
     res.json({ 
